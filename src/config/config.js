32e117c709e9683f32694906cdf42bbef7b3cb16
iannacus <ian.rosas@academlo.com>
2023-08-04 23:10:17 -0600

    fix config file

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
+    username: "iannacus",
+    password: "root",
+    database: "chat_db",
+    port: 5432,
+    host: "localhost",
     dialect: "postgres",
     logging: false,
   },
@@ -18,11 +18,13 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    dialectOptions: {},
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: { ssl: { required: true, rejectUnauthorized: false } },
   },
 };
